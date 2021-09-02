d725d01a16a41d65bd72f4ced56f0a450d573d67
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-02 10:12:22 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,18 +11,14 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://tinyurl.com/yjo8blfx";
+  r_text[0] = "http://telegra.ph/file/68809ff84d7cd4447a76e.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
-*bot setting video : https://youtu.be/VR_ppqEl2q8*
-*githublink       https://github.com/cyberchekuthan/Amalser_v1*
-*audio commads     https://github.com/cyberchekuthan/Amalser_v1/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*DXTROX_V1 BOT CREATED BY AMAL-DX* \n\n *「 ABOUT OWNER 」* \n\n\n *TELEGRAM CHANNEL:- https://t.me/movie_kingdom_org* \n *OWNER NUMBER:- _tinyurl.com/vab4v3es_* \n *GITHUB LINK :- _github.com/amal-dx_* \n\n *「 ABOUT BOT 」* \n\n\n *BOT setting video : loading soon.....* \n\n *Githublink       https://github.com/amal-dx/DXTROX_V1* \n\n *Audio commads     https://github.com/amal-dx/DXTROX_V1/tree/master/uploads* 
 `}) 
 
 }));
